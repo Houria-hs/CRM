95afba288f5b3be51d6c234f1c832aae0eb1f963
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-01-19 23:32:29 +0100

    adding the delete-record functionality

--- CRMapp/urls.py
+++ CRMapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('register/', views.register , name='register' ),
     path('logout/', views.logout_view , name='logout' ),
     path('customer_record/<int:pk>/ ', views.customer_record , name='customer_record' ),
+    path('Delete_record/<int:pk>/', views.Delete_record , name='Delete_record' ),
+    path('Add_record/', views.Add_record , name='Add_record' ),
+    path('update_record/<int:pk>/ ', views.update_record , name='update_record' ),
 ]
--- CRMapp/views.py
+++ CRMapp/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth import logout , login , authenticate
+from django.contrib.auth import logout , login 
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm , AddRecordForm
 from django.contrib import messages
 from .models import Customer 
 
 
 
-# home view
+# home view + the login function
 def home(request):
     customers = Customer.objects.all()
     # log in system
@@ -22,7 +22,7 @@ def home(request):
     return render(request , 'home.html' , {'loginform':loginform , 'customers':customers})
 
 
-# authentication system ( registration , login , logout)
+# authentication system ( registration , logout)
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -39,23 +39,52 @@ def logout_view(request):
     logout(request)
     return redirect('home')
 
+# CRUD operations on customer records
 def customer_record(request , pk):
     if request.user.is_authenticated:
         customer_record = Customer.objects.get(id = pk)
         return render(request , 'indv-record.html' , {'customer_record':customer_record})
     else:
-        messages.warning( request , 'You must be logged in to access this page !')
+        messages.warning( request , 'You must be logged in !')
         return redirect('home')
 
+def Delete_record(request , pk):
+    if request.user.is_authenticated:
+        delete_record = Customer.objects.get(id=pk)
+        delete_record.delete()
+        messages.success(request , 'Record Deleted Successufully')
+        return redirect('home')
+    else:
+        messages.warning( request , 'You must be logged !')
+        return redirect('home')
 
+def Add_record(request):
+    add_record_form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if add_record_form.is_valid():
+                add_record_form.save()
+                messages.success(request , 'Record added successufuly!')
+                return redirect('home')
+            else:
+                messages.success(request, "Please correct the errors below.")
+        return render(request , 'add_record.html' , {"add_record_form":add_record_form})
+    else:
+        messages.success(request , 'You are not authorised for this page , please log In !')
+        redirect('home')
 
-
-
-
-
-
-
-
+def update_record(request , pk):
+    if request.user.is_authenticated:
+        current_record = Customer.objects.get(id = pk)
+        add_record_form = AddRecordForm(request.POST or None , instance = current_record )
+        if add_record_form.is_valid():
+            add_record_form.save()
+            messages.success( request , 'Record updated !')
+            return redirect('home')
+        return render(request , 'update_record.html', {'add_record_form':add_record_form})
+    else:
+        messages.success(request , 'You are not authorised , please Sign-up or Log In !')
+        return redirect('home')
 
 
 
