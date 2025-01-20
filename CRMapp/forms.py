ca5ae954e5eb6dfec5e2baed48e56f1116837c8a
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-01-20 12:18:15 +0100

    add-record functionalities

--- CRMapp/forms.py
+++ CRMapp/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Customer
 
 # the registration form
 class UserRegisterForm(UserCreationForm):
@@ -11,3 +12,12 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = {'username', 'email' , 'password1' , 'password2'}
 
+
+class AddRecordForm(forms.ModelForm):
+    full_name = forms.CharField(required=True , widget=forms.widgets.TextInput(attrs={"placeholder": "full name"}))
+    email = forms.EmailField(required=True , widget=forms.widgets.TextInput(attrs={"placeholder": "email"}))
+    phone = forms.CharField(required=True , widget=forms.widgets.TextInput(attrs={"placeholder": "phone"}))
+    adresse = forms.CharField(required=True , widget=forms.widgets.TextInput(attrs={"placeholder": "adresse"}))
+    class Meta:
+        model = Customer
+        exclude = ('user',)
